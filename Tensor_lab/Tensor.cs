21db213cb79779d43ed732f1b6157d5ecd216464
bonnina <37539513+bonnina@users.noreply.github.com>
2020-05-22 10:37:08 +0300

    Add summaries and params

--- Tensor_lab/Tensor.cs
+++ Tensor_lab/Tensor.cs
@@ -7,7 +7,14 @@ namespace Tensor_lab
     {
         private double[] data;
 
+        /// <summary>
+        /// Shape of the dataset - a matrix, can be from 1D to 3D.
+        /// </summary>
         public int[] Shape { get; set; }
+
+        /// <summary>
+        /// The number of elements the Shape array will hold.
+        /// </summary>
         public int Elements
         {
             get {
@@ -15,17 +22,26 @@ namespace Tensor_lab
             }
         }
 
+        /// <param name="shape"></param>
         public Tensor(params int[] shape)
         {
             Shape = shape;
             data = new double[Elements];
         }
 
+        /// <summary>
+        /// Helper function to load the data intos the Tensor
+        /// </summary>
+        /// <param name="data"></param>
         public void Load(params double[] dataArr)
         {
             data = dataArr;
         }
 
+        /// <summary>
+        /// Fill the array with constant value (e.g. weights or bias)
+        /// </summary>
+        /// <param name="value"></param>
         public void Fill(double value)
         {
             for(int i = 0; i < Elements; i++)
