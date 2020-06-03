379b2fd621dbc5c7eb078b041a94cc0e7c7a1fbd
bonnina <37539513+bonnina@users.noreply.github.com>
2020-06-03 14:38:51 +0300

    Implement > and < operators

--- Tensor_lab/Tensor.cs
+++ Tensor_lab/Tensor.cs
@@ -230,6 +230,42 @@ namespace Tensor_lab
             return t;
         }
 
+        /// <summary>
+        /// Check a > b between corresponding Tensor elements
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Tensor operator >(Tensor a, Tensor b)
+        {
+            Tensor t = new Tensor(a.Shape);
+
+            for (int i = 0; i < a.Elements; i++)
+            {
+                t[i] = a[i] > b[i] ? 1 : 0;
+            }
+
+            return t;
+        }
+
+        /// <summary>
+        /// Check a < b between corresponding Tensor elements
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static Tensor operator <(Tensor a, Tensor b)
+        {
+            Tensor t = new Tensor(a.Shape);
+
+            for (int i = 0; i < a.Elements; i++)
+            {
+                t[i] = a[i] < b[i] ? 1 : 0;
+            }
+
+            return t;
+        }
+
         #endregion
     }
 }
