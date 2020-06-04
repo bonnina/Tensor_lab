ad6a3951f66be2b46ba91ae31cdf1b48cb979dfd
bonnina <37539513+bonnina@users.noreply.github.com>
2020-06-04 21:41:39 +0300

    Create Operations

---
+++ Tensor_lab/Operations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tensor_lab
+{
+    class Operations
+    {
+        /// <summary>
+        /// Creates a tensor with random data (declaring weights).
+        /// </summary>
+        /// <param name="shape"></param>
+        /// <returns></returns>
+        public Tensor GetRandom(params int[] shape)
+        {
+            var tensor = new Tensor();
+            var random = new Random();
+
+            for (int i = 0; i < tensor.Elements; i++) {
+                tensor[i] = random.NextDouble();
+            }
+
+            return tensor;
+        }
+    }
+}
