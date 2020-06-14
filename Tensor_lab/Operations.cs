429b1f388ddf296681f6321b4dbbc824dbc4ec34
bonnina <37539513+bonnina@users.noreply.github.com>
2020-06-14 20:40:42 +0300

    Add GetDotProduct

--- Tensor_lab/Operations.cs
+++ Tensor_lab/Operations.cs
@@ -34,5 +34,35 @@ namespace Tensor_lab
 
             return result;
         }
+
+        /// <summary>
+        /// Perform the dot product between two matrices. Only applicable to 2D Tensor. 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public Tensor GetDotProduct(Tensor a, Tensor b)
+        {
+            if (a.Shape[1] != b.Shape[0])
+            {
+                throw new Exception("a->Cols must be equal to b->Rows");
+            }
+            int m = a.Shape[0];
+            int q = b.Shape[1];
+            int n = a.Shape[1];
+            Tensor r = new Tensor(m, q);
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < q; j++)
+                {
+                    r[i, j] = 0;
+                    for (int k = 0; k < n; k++)
+                    {
+                        r[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            return r;
+        }
     }
 }
