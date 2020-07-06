a1fc475d39e90f8ef4dab511f1b7bd225b6f76dc
bonnina <37539513+bonnina@users.noreply.github.com>
2020-07-06 20:54:07 +0300

    Create BaseLayer

---
+++ Tensor_lab/Layers/BaseLayer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tensor_lab.Layers
+{
+    public abstract class BaseLayer : Operations
+    {
+        public string Name { get; set; }
+
+        public Tensor Input { get; set; }
+
+        public Tensor Output { get; set; }
+
+        public Dictionary<string, Tensor> Parameters { get; set; }
+
+        public BaseLayer(string name)
+        {
+            Name = name;
+            Parameters = new Dictionary<string, Tensor>();
+        }
+        
+        public virtual void Forward(Tensor t)
+        {
+            Input = t;
+        }
+    }
+}
