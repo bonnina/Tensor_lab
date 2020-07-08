066d14f0e686dd9eaea8c5bdf44284734a3d5d3e
bonnina <37539513+bonnina@users.noreply.github.com>
2020-07-08 17:03:16 +0300

    Add summaries

--- Tensor_lab/Layers/BaseLayer.cs
+++ Tensor_lab/Layers/BaseLayer.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Tensor_lab.Layers
 {
     public abstract class BaseLayer : Operations
     {
+        /// <summary>
+        /// Layer name
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Input for the layer
+        /// </summary>
         public Tensor Input { get; set; }
 
+        /// <summary>
+        /// Output after forwarding the input across the neurons
+        /// </summary>
         public Tensor Output { get; set; }
 
+        /// <summary>
+        /// Trainable parameters list, eg, weight, bias
+        /// </summary>
         public Dictionary<string, Tensor> Parameters { get; set; }
 
         public BaseLayer(string name)
@@ -20,6 +30,10 @@ namespace Tensor_lab.Layers
             Parameters = new Dictionary<string, Tensor>();
         }
         
+        /// <summary>
+        /// Virtual forward method to perform calculation and move the input to next layer
+        /// </summary>
+        /// <param name="x"></param>
         public virtual void Forward(Tensor t)
         {
             Input = t;
