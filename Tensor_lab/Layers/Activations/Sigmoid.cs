4b7582a6b43971f3e4c57b2690b94e38488fe680
bonnina <37539513+bonnina@users.noreply.github.com>
2020-07-14 13:43:37 +0300

    Implement Sigmoid

--- Tensor_lab/Layers/Activations/Sigmoid.cs
+++ Tensor_lab/Layers/Activations/Sigmoid.cs
@@ -4,5 +4,11 @@ namespace Tensor_lab.Layers.Activations
     public class Sigmoid : BaseActivation
     {
         public Sigmoid() : base("sigmoid") {}
+
+        public override void Forward(Tensor x)
+        {
+            base.Forward(x);
+            Output = GetExp(x) / (1 + GetExp(x));
+        }
     }
 }
