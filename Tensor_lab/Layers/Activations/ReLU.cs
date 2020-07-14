39e29fc18715ed075e57f00feb83856dbb751023
bonnina <37539513+bonnina@users.noreply.github.com>
2020-07-14 13:46:00 +0300

    Implement ReLU

--- Tensor_lab/Layers/Activations/ReLU.cs
+++ Tensor_lab/Layers/Activations/ReLU.cs
@@ -4,5 +4,13 @@ namespace Tensor_lab.Layers.Activations
     public class ReLU : BaseActivation
     {
         public ReLU() : base("relu") { }
+
+        public override void Forward(Tensor x)
+        {
+            base.Forward(x);
+
+            Tensor matches = x > 0;
+            Output = matches * x;
+        }
     }
 }
