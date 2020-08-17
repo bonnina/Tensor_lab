fe66a9c9e8229e74a0fdf8517c31c947c2543230
bonnina <37539513+bonnina@users.noreply.github.com>
2020-08-17 23:23:52 +0300

    Impl FullyConnectedLayer

---
+++ Tensor_lab/Layers/FullyConnectedLayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Tensor_lab.Layers.Activations;
+
+namespace Tensor_lab.Layers
+{
+    public class FullyConnectedLayer : BaseLayer
+    {
+        public int InputDim { get; set; }
+        public int OutNeurons { get; set; }
+        public BaseActivation Activation { get; set; }
+
+        public FullyConnectedLayer(int input_dim, int output_neurons, string act) : base("fc")
+        {
+            Parameters["w"] = GetRandom(input_dim, output_neurons);
+            InputDim = input_dim;
+            OutNeurons = output_neurons;
+            Activation = BaseActivation.Get(act);
+        }
+        public override void Forward(Tensor x)
+        {
+            base.Forward(x);
+            Output = GetDotProduct(x, Parameters["w"]);
+            if (Activation != null)
+            {
+                Activation.Forward(Output);
+                Output = Activation.Output;
+            }
+        }
+    }
+}
