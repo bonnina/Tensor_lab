55242acd9d8ec31daca9bcb1512281c2963be8bf
bonnina <37539513+bonnina@users.noreply.github.com>
2020-08-30 21:48:44 +0300

    Define base cost function

---
+++ Tensor_lab/CostFuncs/BaseCost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tensor_lab.CostFuncs
+{
+    public abstract class BaseCost : Operations
+    {
+        public string Name { get; set; }
+
+        public BaseCost(string name)
+        {
+            Name = name;
+        }
+
+        public abstract Tensor Forward(Tensor preds, Tensor labels);
+    }
+}
