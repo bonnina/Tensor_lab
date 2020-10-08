b9cb3a95500b8c0f71ef2a9e9469696e874d408d
bonnina <37539513+bonnina@users.noreply.github.com>
2020-10-08 23:30:49 +0300

    Define BinaryCrossEntropy

---
+++ Tensor_lab/CostFuncs/BinaryCrossEntropy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tensor_lab.CostFuncs
+{
+    public class BinaryCrossEntropy : BaseCost
+    {
+        public BinaryCrossEntropy() : base("binary_crossentropy"){}
+        public override Tensor Forward(Tensor preds, Tensor labels)
+        {
+            var output = Clip(preds, Epsilon, 1 - Epsilon);
+            output = Mean(-(labels * GetLog(output) + (1 - labels) * GetLog(1 - output)));
+            return output;
+        }
+    }
+}
