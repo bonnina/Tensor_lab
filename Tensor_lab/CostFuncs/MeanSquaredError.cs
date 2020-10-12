68348792cdd0ffc0849728a2225e586564ef8629
bonnina <37539513+bonnina@users.noreply.github.com>
2020-10-12 11:47:29 +0300

    Add MeanSquaredError

---
+++ Tensor_lab/CostFuncs/MeanSquaredError.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tensor_lab.CostFuncs
+{
+    public class MeanSquaredError : BaseCost
+    {
+        public MeanSquaredError() : base("mean_squared_error")
+        {
+
+        }
+        public override Tensor Forward(Tensor preds, Tensor labels)
+        {
+            var error = preds - labels;
+            return Mean(GetPow(error));
+        }
+    }
+}
