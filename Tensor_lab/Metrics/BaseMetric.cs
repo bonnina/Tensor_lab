57f21ea98fcaa21756461bf62e8e9bc9462eb6a4
bonnina <37539513+bonnina@users.noreply.github.com>
2020-10-18 19:43:59 +0300

    Create Metrics

---
+++ Tensor_lab/Metrics/BaseMetric.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tensor_lab.Metrics
+{
+    public abstract class BaseMetric : Operations
+    {
+        public string Name { get; set; }
+
+        public BaseMetric(string name)
+        {
+            Name = name;
+        }
+
+        public abstract Tensor Calculate(Tensor preds, Tensor labels);
+    }
+}
