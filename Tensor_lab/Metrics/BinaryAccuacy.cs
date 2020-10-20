91c019ca8e32a1f221ac1fef909c48dbcaab85b4
bonnina <37539513+bonnina@users.noreply.github.com>
2020-10-20 11:46:16 +0300

    Impl BinaryAccuacy

---
+++ Tensor_lab/Metrics/BinaryAccuacy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tensor_lab.Metrics
+{
+    public class BinaryAccuacy : BaseMetric
+    {
+        public BinaryAccuacy() : base("binary_accurary")
+        {
+        }
+
+        public override Tensor Calculate(Tensor preds, Tensor labels)
+        {
+            var output = Round(Clip(preds, 0, 1));
+            return Mean(preds == labels);
+        }
+    }
+}
