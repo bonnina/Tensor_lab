c473ef87d3c52319c0f48c53b749142a198718ed
bonnina <37539513+bonnina@users.noreply.github.com>
2020-10-23 17:14:35 +0300

    Impl Mean Absolute Error

---
+++ Tensor_lab/Metrics/MeanAbsoluteError.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tensor_lab.Metrics
+{
+    public class MeanAbsoluteError : BaseMetric
+    {
+        public MeanAbsoluteError() : base("mean_absolute_error")
+        {
+
+        }
+
+        public override Tensor Calculate(Tensor preds, Tensor labels)
+        {
+            var error = preds - labels;
+            return Mean(Abs(error));
+        }
+    }
+}
