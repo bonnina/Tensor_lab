805b79c4385b054a1811a5690f2678625f64540d
bonnina <37539513+bonnina@users.noreply.github.com>
2020-10-26 23:10:46 +0200

    Update test

--- Tensor_lab.Test/Program.cs
+++ Tensor_lab.Test/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Tensor_lab.Layers;
+using Tensor_lab.Metrics;
+using Tensor_lab.CostFuncs;
 
 namespace Tensor_lab.Test
 {
@@ -7,22 +9,38 @@ namespace Tensor_lab.Test
     {
         static void Main(string[] args)
         {
+            Operations K = new Operations();
+
             //Load array to the tensor
-            Tensor x = new Tensor(1, 3);
-            x.Load(1, 2, 3);
-            x.Print("Load array");
+            Tensor x = new Tensor(3, 3);
+            x.Load(2, 4, 6, 1, 3, 5, 2, 3, 5);
+            x.Print("Load X Values");
+
+            Tensor y = new Tensor(3, 1);
+            y.Load(20, 15, 15);
+            y.Print("Load Y Values");
+
+            K.Mean(y, 0).Print();
 
             //Create two layers, one with 6 neurons and another with 1
             FullyConnectedLayer fc1 = new FullyConnectedLayer(3, 6, "relu");
-            FullyConnectedLayer fc2 = new FullyConnectedLayer(6, 1, "sigmoid");
+            FullyConnectedLayer fc2 = new FullyConnectedLayer(6, 1, "relu");
 
             //Connect input by passing data from one layer to another
             fc1.Forward(x);
-            x = fc1.Output;
-            x.Print("FC1 Output");
-            fc2.Forward(x);
-            x = fc2.Output;
-            x.Print("FC2 Output");
+            fc2.Forward(fc1.Output);
+            var preds = fc2.Output;
+            preds.Print("Predictions");
+
+            //Calculate the mean square error cost between the predicted and expected values
+            BaseCost cost = new MeanSquaredError();
+            var costValues = cost.Forward(preds, y);
+            costValues.Print("MSE Cost");
+
+            //Calculate the mean absolute metric value for the predicted vs expected values
+            BaseMetric metric = new MeanAbsoluteError();
+            var metricValues = metric.Calculate(preds, y);
+            metricValues.Print("MAE Metric");
 
             Console.ReadLine();
         }
