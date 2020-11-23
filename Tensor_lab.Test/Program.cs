19a27cadc791811dfe8423d19a743f0167248e2c
bonnina <37539513+bonnina@users.noreply.github.com>
2020-11-23 23:46:13 +0200

    Update test

--- Tensor_lab.Test/Program.cs
+++ Tensor_lab.Test/Program.cs
@@ -17,14 +17,12 @@ namespace Tensor_lab.Test
             x.Print("Load X Values");
 
             Tensor y = new Tensor(3, 1);
-            y.Load(20, 15, 15);
+            y.Load(1, 0, 1);
             y.Print("Load Y Values");
-
-            K.Mean(y, 0).Print();
 
             //Create two layers, one with 6 neurons and another with 1
             FullyConnectedLayer fc1 = new FullyConnectedLayer(3, 6, "relu");
-            FullyConnectedLayer fc2 = new FullyConnectedLayer(6, 1, "relu");
+            FullyConnectedLayer fc2 = new FullyConnectedLayer(6, 1, "sigmoid");
 
             //Connect input by passing data from one layer to another
             fc1.Forward(x);
@@ -33,14 +31,28 @@ namespace Tensor_lab.Test
             preds.Print("Predictions");
 
             //Calculate the mean square error cost between the predicted and expected values
-            BaseCost cost = new MeanSquaredError();
+            BaseCost cost = new BinaryCrossEntropy();
             var costValues = cost.Forward(preds, y);
-            costValues.Print("MSE Cost");
+            costValues.Print("BCE Cost");
 
             //Calculate the mean absolute metric value for the predicted vs expected values
-            BaseMetric metric = new MeanAbsoluteError();
+            BaseMetric metric = new BinaryAccuacy();
             var metricValues = metric.Calculate(preds, y);
-            metricValues.Print("MAE Metric");
+            metricValues.Print("Acc Metric");
+
+            //Backpropagtion starts here.
+            //Calculate gradient of the cost function.
+            var grad = cost.Backward(preds, y);
+
+            //Then the FC2 layer by passing the cost function grad into the layer backward function
+            fc2.Backward(grad);
+
+            //The grad of the FC2 is stored in the InputGrad property, we need to pass it to the next layer.
+            fc1.Backward(fc2.InputGrad);
+
+            //Lets print the parameters for both the layers along with the Grad
+            fc1.PrintParams();
+            fc2.PrintParams();
 
             Console.ReadLine();
         }
